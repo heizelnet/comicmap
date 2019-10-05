d651287b1795a21e43f17bbd7dd1fd62607eacfa
heizelnet <heizelnet@gmail.com>
2019-10-05 20:54:00 +0900

    UI / UX modify

--- app/src/main/java/com/example/comicmap/MainActivity.java
+++ app/src/main/java/com/example/comicmap/MainActivity.java
@@ -12,7 +12,6 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.LinearLayout;
-import android.widget.Toast;
 
 import com.google.android.material.navigation.NavigationView;
 
